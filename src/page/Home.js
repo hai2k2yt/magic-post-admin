0b8ba84981846d9da0f039150455d0f777391273
rinchan01 <thailinh8426@gmail.com>
2023-11-21 21:25:30 +0700

    update navbar, register form

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import Navbar from './auth/Navbar';
+import Navbar from './components/Navbar';
 
 export default function Home() {
     return (
