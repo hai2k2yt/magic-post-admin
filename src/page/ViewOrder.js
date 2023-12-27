f51c258601e59b7a7e9cad7045026cf3fdc93cd3
haith <haithachxa2002@gmail.com>
2023-12-27 12:17:43 +0700

    match api view order

--- src/page/ViewOrder.js
+++ src/page/ViewOrder.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Box, Paper, Typography, Grid, Button } from '@mui/material';
+import React, {useEffect, useState} from 'react';
+import {Box, Paper, Typography, Grid, Button} from '@mui/material';
 import Timeline from '@mui/lab/Timeline';
 import TimelineItem from '@mui/lab/TimelineItem';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
@@ -8,9 +8,12 @@ import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 import MainNavbar from '../component/layout/MainNavbar';
 import Footer from '../component/layout/Footer';
-import { createTheme } from '@mui/material/styles';
-import { ThemeProvider } from '@emotion/react';
+import {createTheme} from '@mui/material/styles';
+import {ThemeProvider} from '@emotion/react';
 import PropTypes from 'prop-types';
+import {listP2PGatheringOrders} from "../api/transport";
+import {getOrder} from "../api/order";
+
 const theme = createTheme({
     typography: {
         "fontFamily": '"Montserrat", "sans-serif"',
@@ -26,179 +29,207 @@ const theme = createTheme({
     }
 
 })
+
 function Item(props) {
-    const { sx, ...other } = props;
+    const {sx, ...other} = props;
     return (
-      <Box
-        sx={{
-          p: 1,
-          m: 1,
-          fontSize: '0.875rem',
-          fontWeight: '700',
-          ...sx,
-        }}
-        {...other}
-      />
+        <Box
+            sx={{
+                p: 1,
+                m: 1,
+                fontSize: '0.875rem',
+                fontWeight: '700',
+                ...sx,
+            }}
+            {...other}
+        />
     );
-  }
-  
-  Item.propTypes = {
+}
+
+Item.propTypes = {
     /**
      * The system prop that allows defining system overrides as well as additional CSS styles.
      */
     sx: PropTypes.oneOfType([
-      PropTypes.arrayOf(
-        PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool]),
-      ),
-      PropTypes.func,
-      PropTypes.object,
+        PropTypes.arrayOf(
+            PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool]),
+        ),
+        PropTypes.func,
+        PropTypes.object,
     ]),
-  };
+};
 const ViewOrder = () => {
+    const [orderId, setOrderId] = useState('')
+    const [order, setOrderDetail] = useState(null)
+
+    const fetchOrderDetail = async () => {
+        if (orderId) {
+            try {
+                const response = await getOrder(orderId);
+                setOrderDetail(response);
+            } catch (e) {
+                console.log(e)
+            }
+        } else {
+            setOrderDetail(null)
+        }
+    }
+
     return (
         <ThemeProvider theme={theme}>
-
-            <MainNavbar />
-            <Box sx={{ display: 'flex', justifyContent: 'flex-start', marginTop:'40px', marginBottom:'-20px' }} >
+            <MainNavbar/>
+            <Box sx={{display: 'flex', justifyContent: 'flex-start', marginTop: '40px', marginBottom: '-20px'}}>
                 <Item>
-                    <Typography sx={{ marginLeft: { lg: '40px', md: '40px', sm: '10px', sx: '10px' }, fontWeight:'bold' }} variant='h5' >Tìm kiếm đơn hàng: </Typography>
+                    <Typography sx={{marginLeft: {lg: '40px', md: '40px', sm: '10px', sx: '10px'}, fontWeight: 'bold'}}
+                                variant='h5'>Tìm kiếm đơn hàng: </Typography>
                 </Item>
                 <Item>
-                    <input type="text" class="mr-10 w-11/12 rounded-lg" placeholder="Nhập mã đơn hàng" />
-
+                    <input
+                        value={orderId}
+                        onChange={e => setOrderId(e.target.value)}
+                        type="text" class="mr-10 w-11/12 rounded-lg" placeholder="Nhập mã đơn hàng"/>
                 </Item>
                 <Item>
-                    <Button variant="contained" style={{ backgroundColor: '#31304D', color: '#ffffff' }}>Tìm kiếm</Button>
+                    <Button onClick={fetchOrderDetail} variant="contained"
+                            style={{backgroundColor: '#31304D', color: '#ffffff'}}>Tìm kiếm</Button>
                 </Item>
             </Box>
-            <Box p={2} >
-                <Paper elevation={3} sx={{ padding: '20px', margin: { lg: '40px', md: '40px', sm: '10px', sx: '10px' } }}>
-                    <Typography variant="h5" gutterBottom>
-                        Người gửi
-                    </Typography>
-                    <Grid container spacing={2}>
-                        <Grid item xs={4}>
-                            <Typography>Họ tên:</Typography>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Typography>Điện thoại:</Typography>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Typography>Mã khách hàng:</Typography>
-                        </Grid>
-                    </Grid>
-
-                    <Typography variant="h5" gutterBottom style={{ marginTop: '20px' }}>
-                        Điểm giao dịch
-                    </Typography>
-                    <Grid container spacing={2}>
-                        <Grid item xs={4}>
-                            <Typography>Tên điểm:</Typography>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Typography>Địa chỉ:</Typography>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Typography>Mã bưu chính:</Typography>
-                        </Grid>
-                    </Grid>
-
-                    <Typography variant="h5" gutterBottom style={{ marginTop: '20px' }}>
-                        Order
-                    </Typography>
-                    <Grid container spacing={2}>
-                        <Grid item xs={6}>
-                            <Typography>Loại hàng gửi:</Typography>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Typography>Dịch vụ cộng thêm:</Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Typography>Chỉ dẫn khi không phát được bưu gửi:</Typography>
-                        </Grid>
-                    </Grid>
-
-                    <Typography variant="h5" gutterBottom style={{ marginTop: '20px' }}>
-                        Cam kết
-                    </Typography>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <Typography>Nội dung:</Typography>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Typography>Ngày giờ gửi:</Typography>
-                        </Grid>
-                    </Grid>
-
-                    <Typography variant="h5" gutterBottom style={{ marginTop: '20px' }}>
-                        Cước
-                    </Typography>
-                    <Grid container spacing={2}>
-                        <Grid item xs={4}>
-                            <Typography style={{ marginBottom: '20px' }}>Người gửi</Typography>
-                            <Typography>Cước chính:</Typography>
-                            <Typography>Phụ phí:</Typography>
-                            <Typography>Tổng cước:</Typography>
-                            <Typography>Cước GTGT:</Typography>
-                            <Typography>Tổng cước (gồm VAT):</Typography>
-                            <Typography>Thu khác:</Typography>
-                            <Typography>Tổng thu:</Typography>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Typography style={{ marginBottom: '20px' }}>Người nhận</Typography>
-                            <Typography>COD:</Typography>
-                            <Typography>Thu khác:</Typography>
-                            <Typography>Tổng thu:</Typography>
+            {order && (
+                <Box p={2}>
+                    <Paper elevation={3}
+                           sx={{padding: '20px', margin: {lg: '40px', md: '40px', sm: '10px', sx: '10px'}}}>
+                        <Typography variant="h5" gutterBottom>
+                            Người gửi
+                        </Typography>
+                        <Grid container spacing={2}>
+                            <Grid item xs={6}>
+                                <Typography>Họ tên: {order?.sender?.name}</Typography>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Typography>Điện thoại: {order?.sender?.phone}</Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Typography>Mã khách hàng: {order?.sender?.id}</Typography>
+                            </Grid>
                         </Grid>
 
-                    </Grid>
-
-                    <Typography variant="h5" gutterBottom style={{ marginTop: '20px' }}>
-                        Khối lượng
-                    </Typography>
-                    <Grid container spacing={2}>
-                        <Grid item xs={4}>
-                            <Typography>Khối lượng thực tế:</Typography>
+                        <Typography variant="h5" gutterBottom style={{marginTop: '20px'}}>
+                            Điểm giao dịch
+                        </Typography>
+                        <Grid container spacing={2}>
+                            <Grid item xs={4}>
+                                <Typography>Tên điểm: </Typography>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Typography>Địa chỉ: {order?.sender?.address?.street}</Typography>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Typography>Mã bưu chính: {order?.sender?.address?.zipcode}</Typography>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={4}>
-                            <Typography>Khối lượng quy đổi:</Typography>
+
+                        <Typography variant="h5" gutterBottom style={{marginTop: '20px'}}>
+                            Order
+                        </Typography>
+                        <Grid container spacing={2}>
+                            <Grid item xs={6}>
+                                <Typography>Loại hàng gửi: {order?.type}</Typography>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Typography>Dịch vụ cộng thêm: {order?.specialService}</Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Typography>Chỉ dẫn khi không phát được bưu gửi: {order?.senderInstructor}</Typography>
+                            </Grid>
                         </Grid>
-                    </Grid>
 
-                    <Typography variant="h5" gutterBottom style={{ marginTop: '20px' }}>
-                        QR code
-                    </Typography>
-                    <Grid container spacing={2}>
-                        {/* Add QR code label here */}
-                    </Grid>
+                        <Typography variant="h5" gutterBottom style={{marginTop: '20px'}}>
+                            Cam kết
+                        </Typography>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Typography>Nội dung:</Typography>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Typography>Ngày giờ gửi: {order?.sendTime}</Typography>
+                            </Grid>
+                        </Grid>
 
-                    <Timeline position="alternate">
-                        <TimelineItem>
-                            <TimelineSeparator>
-                                <TimelineDot />
-                                <TimelineConnector />
-                            </TimelineSeparator>
-                            <TimelineContent>
-                                <Typography variant="body2">Thời gian 1</Typography>
-                                <Typography variant="body2" color="textSecondary">Message 1</Typography>
-                            </TimelineContent>
-                        </TimelineItem>
-                        <TimelineItem>
-                            <TimelineSeparator>
-                                <TimelineDot />
-                                <TimelineConnector />
-                            </TimelineSeparator>
-                            <TimelineContent>
-                                <Typography variant="body2">Thời gian 2</Typography>
-                                <Typography variant="body2" color="textSecondary">Message 2</Typography>
-                            </TimelineContent>
-                        </TimelineItem>
-                        {/* Add more TimelineItems as needed */}
-                    </Timeline>
-                </Paper>
-            </Box>
-            <Footer />
-        </ThemeProvider >
+                        <Typography variant="h5" gutterBottom style={{marginTop: '20px'}}>
+                            Cước
+                        </Typography>
+                        <Grid container spacing={2}>
+                            <Grid item xs={4}>
+                                <Typography style={{marginBottom: '20px'}}>Người gửi</Typography>
+                                <Typography>Cước chính: {order?.price?.mainTax}</Typography>
+                                <Typography>Phụ phí: {order?.price?.subTax}</Typography>
+                                <Typography>Phí vận chuyển: {order?.price?.transportTax}</Typography>
+                                <Typography>Tổng phí: {order?.price?.totalTax}</Typography>
+                                <Typography>Thu khác: {order?.price?.otherTax}</Typography>
+                                <Typography>Tổng thu: {order?.price?.totalPrice}</Typography>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Typography style={{marginBottom: '20px'}}>Người nhận</Typography>
+                                <Typography>COD: {order?.cashOnDelivery?.cod}</Typography>
+                                <Typography>Thu khác: {order?.cashOnDelivery?.other}</Typography>
+                                <Typography>Tổng thu: {order?.cashOnDelivery?.totalCOD}</Typography>
+                            </Grid>
+
+                        </Grid>
+
+                        <Typography variant="h5" gutterBottom style={{marginTop: '20px'}}>
+                            Khối lượng
+                        </Typography>
+                        <Grid container spacing={2}>
+                            <Grid item xs={4}>
+                                <Typography>Khối lượng thực tế: {order?.actualWeight}</Typography>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Typography>Khối lượng quy đổi: {order?.volumetricWeight}</Typography>
+                            </Grid>
+                        </Grid>
+
+                        <Typography variant="h5" gutterBottom style={{marginTop: '20px'}}>
+                            QR code
+                        </Typography>
+                        <Grid container spacing={2}>
+                            {/* Add QR code label here */}
+                        </Grid>
+
+                        <Typography variant="h5" gutterBottom style={{marginTop: '20px'}}>
+                            Trạng thái đơn hàng: {order.status}
+                        </Typography>
+
+
+                        <Timeline position="alternate">
+                            <TimelineItem>
+                                <TimelineSeparator>
+                                    <TimelineDot/>
+                                    <TimelineConnector/>
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <Typography variant="body2">Thời gian 1</Typography>
+                                    <Typography variant="body2" color="textSecondary">Message 1</Typography>
+                                </TimelineContent>
+                            </TimelineItem>
+                            <TimelineItem>
+                                <TimelineSeparator>
+                                    <TimelineDot/>
+                                    <TimelineConnector/>
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <Typography variant="body2">Thời gian 2</Typography>
+                                    <Typography variant="body2" color="textSecondary">Message 2</Typography>
+                                </TimelineContent>
+                            </TimelineItem>
+                            {/* Add more TimelineItems as needed */}
+                        </Timeline>
+                    </Paper>
+                </Box>
+            )}
+
+            <Footer/>
+        </ThemeProvider>
     );
 };
 
